3cb238331189a78c9631ebf6485eb0af729aedcf
goldfish <81166150+HUANGgame@users.noreply.github.com>
2025-06-05 15:43:50 +0800

    Update MusicService.kt

--- app/src/main/java/com/example/bubbletalk20/MusicService.kt
+++ app/src/main/java/com/example/bubbletalk20/MusicService.kt
@@ -14,30 +14,30 @@ class MusicService : Service() {
         super.onCreate()
 
         mediaPlayer = MediaPlayer.create(this, R.raw.goodnight).apply {
-            // 2. 設定為無限循環播放
+          
             isLooping = true
-            // 3. 設定適當的音量，例如 50%
+            
             setVolume(0.3f, 0.3f)
-            // 4. 開始播放
+          
             start()
         }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // 當 Service 被系統意外殺掉後要重啟所使用的旗標
+        
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // 5. 停止並釋放 MediaPlayer
+       
         mediaPlayer?.stop()
         mediaPlayer?.release()
         mediaPlayer = null
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        // 這是一個純粹用 startService()/stopService() 的 Service，不需要 bind
+       
         return null
     }
 }
